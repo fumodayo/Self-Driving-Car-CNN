c3d6e3acb0bcf0d190e4360ebe6b48b9d46a0628
thairyo <sonthai1310.works@gmail.com>
2022-08-09 13:31:59 +0700

    Road definition.

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+
+// adjust the position of car to Road[0,1,2]
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -14,6 +17,14 @@ function animate() {
 
   // resize lock height canvas
   canvas.height = window.innerHeight;
+
+  // adjust the position of car to 0.7 canvas
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
+  road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
